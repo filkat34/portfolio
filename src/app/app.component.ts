95776b2e7485ffe99f8f127a3c844c503802880c
filippos <filippos29@hotmail.com>
2025-05-12 23:21:21 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RouterLink,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'Portfolio F. Katsanos';
+  constructor(private router: Router) {}
+
+  hover: boolean = false; // État du survol
+
+  contactIcons = [
+    {
+      href: 'https://github.com/filkat34',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      class: 'fab fa-github fa-xl',
+      color: 'text-black'
+    },
+    {
+      href: 'mailto:filippos.katsanos@protonmail.com',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      class: 'fas fa-envelope fa-xl',
+      color: 'text-black'
+    },
+    {
+      href: 'https://www.linkedin.com/in/fkatsanos/',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      class: 'fab fa-linkedin fa-xl',
+      color: 'text-black'
+    },
+  ];
+
+
+ isHomeRouteActive(): boolean {
+    return this.router.url === '/home';
+  }
+
+  getAccueilText(): string {
+  if (!this.isHomeRouteActive()) {
+    return this.hover ? '&lt;Accueil/&gt;' : '&lt;/&gt;';
+  }
+  return '&lt;Accueil/&gt;';
+
+}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CvComponent } from './cv/cv.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'cv', component: CvComponent },
+    { path: 'projets', component: ProjectsComponent}
+];
