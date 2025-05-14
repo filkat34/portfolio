759b3d49e4227e06f0256ba7a94b6bd6e916dee3
filippos <filippos29@hotmail.com>
2025-05-14 21:16:21 +0200

    darkmode update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Portfolio F. Katsanos';
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   hover: boolean = false; // État du survol
 
@@ -19,35 +19,49 @@ export class AppComponent {
       href: 'https://github.com/filkat34',
       target: '_blank',
       rel: 'noopener noreferrer',
-      class: 'fab fa-github fa-xl',
+      class: 'fab fa-github fa-l',
       color: 'text-black'
     },
     {
       href: 'mailto:filippos.katsanos@protonmail.com',
       target: '_blank',
       rel: 'noopener noreferrer',
-      class: 'fas fa-envelope fa-xl',
+      class: 'fas fa-envelope fa-l',
       color: 'text-black'
     },
     {
       href: 'https://www.linkedin.com/in/fkatsanos/',
       target: '_blank',
       rel: 'noopener noreferrer',
-      class: 'fab fa-linkedin fa-xl',
+      class: 'fab fa-linkedin fa-l',
       color: 'text-black'
     },
   ];
 
 
- isHomeRouteActive(): boolean {
+  isHomeRouteActive(): boolean {
     return this.router.url === '/home';
   }
 
   getAccueilText(): string {
-  if (!this.isHomeRouteActive()) {
-    return this.hover ? '&lt;Accueil/&gt;' : '&lt;/&gt;';
+    if (!this.isHomeRouteActive()) {
+      return this.hover ? '&lt;Accueil/&gt;' : '&lt;/&gt;';
+    }
+    return '&lt;Accueil/&gt;';
   }
-  return '&lt;Accueil/&gt;';
 
-}
+  isDarkMode = false; // Par défaut, le mode clair est activé
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    const htmlElement = document.documentElement;
+
+    if (this.isDarkMode) {
+      htmlElement.classList.add('dark'); // Ajoute la classe "dark" pour activer le mode sombre
+      console.log('Mode sombre activé');
+    } else {
+      htmlElement.classList.remove('dark'); // Supprime la classe "dark" pour revenir au mode clair
+      console.log('Mode sombre désactivé')
+    };
+  }
 }
