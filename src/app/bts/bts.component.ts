f4c40658ddeca548294ee16c9adbc8ce03cc75f1
filippos-stagiaire <filippos@pyxiscience.com>
2025-07-20 18:33:44 +0200

    portfolio update

---
+++ src/app/bts/bts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bts',
+    imports: [
+    ],
+  templateUrl: './bts.component.html',
+  styleUrl: './bts.component.css'
+})
+export class BtsComponent {
+
+}
